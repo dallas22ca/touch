5ad941c7642b88785c0c4729471ed68662f3277d
Dallas Read <dallas@excitereative.ca>
2014-05-06 12:06:17 -0300

    Gooda.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :organization
+  has_many :meetings
+  
+  validates_presence_of :organization_id, :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   
-  resources :meetings
-
   devise_for :users
   
   get "/:permalink/contacts" => "modules#contacts", as: :contacts
   get "/:permalink/attendance" => "modules#attendance", as: :attendance
   
   scope "/:permalink" do
-    resources :rooms, path: :attendance
+    resources :rooms, path: :attendance do
+      resources :meetings
+    end
   end
   
   root to: "modules#redirect"
