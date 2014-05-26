8291f0cdc7a60f578c019e0a1fe92095ccc413e1
Dallas Read <dallas@excitereative.ca>
2014-05-26 10:27:06 -0300

    Gr.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,7 @@ class Task < ActiveRecord::Base
   
   validates_presence_of :creator, :content
   
-  scope :by_ordinal, -> { order "tasks.ordinal asc, tasks.updated_at asc" }
+  scope :by_ordinal, -> { order "tasks.ordinal asc" }
   scope :complete, -> { where complete: true }
   scope :incomplete, -> { where complete: false }
   scope :by_completed_at, -> { order "tasks.updated_at desc" }
