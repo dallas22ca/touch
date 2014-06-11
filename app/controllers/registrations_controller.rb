c196f981ea4b9232a8302743fec8bc6efa710f56
Dallas Read <dallas@excitereative.ca>
2014-06-11 13:32:40 -0300

    Remember me fix.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,6 +21,7 @@ class RegistrationsController < Devise::RegistrationsController
     resource_saved = resource.save
     yield resource if block_given?
     if resource_saved
+      resource.remember_me!
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,6 +24,7 @@ class SessionsController < Devise::SessionsController
       resource.accept_invitation
     end
     
+    resource.remember_me!
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
